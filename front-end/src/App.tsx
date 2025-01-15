c2482128b667f1c59550b703e92dc45576a39800
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-15 22:04:06 +0800

    added to do list

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,16 @@
 import { useEffect, useState } from "react";
 
+/*
+  TO DO
+
+  - Implement simple fetch / get function and map to screen
+  - Implement post function
+  - Implement put and place functions
+  - implement delete
+  - play wiht file handling
+  - use other python modules
+*/
+
 function App() {
   useEffect(() => {
     console.log(import.meta.env.VITE_API_URL);
