7f7c073d21606cbec451a17969b629d51fcbdee8
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-17 10:13:31 +0800

    form to submit post

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,18 +3,24 @@ import { useEffect, useState } from "react";
 /*
   TO DO
 
-  - Implement simple fetch / get function and map to screen
-  - Implement post function
-  - Implement put and place functions
-  - implement delete
-  - play wiht file handling
-  - use other python modules
+  [/] Implement simple fetch / get function and map to screen
+  [] Implement post function
+  [] Implement put and place functions
+  [] implement delete
+  [] play wiht file handling
+  [] use other python modules
 */
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
 function App() {
-  const [data, setData] = useState([
+  const [data, setData] = useState<Post[]>([
     {
       id: 0,
       title: "",
@@ -40,21 +46,32 @@ function App() {
   }, []);
 
   return (
-    <div className="flex items-center justify-center flex-col space-y-2 h-screen align-middle">
-      {data.length > 0 ? (
-        data.map((item) => (
-          <div className="flex text-white font-semibold font-serif flex-col bg-zinc-800 p-11 rounded-2xl">
-            <div className="flex" key={item.id}>
-              {item.title}
+    <div className="main flex mx-16">
+      <div className="flex flex-[2] items-center justify-center flex-col space-y-2 h-screen align-middle">
+        {data.length > 0 ? (
+          data.map((item) => (
+            <div className="flex text-white font-semibold font-serif flex-col bg-zinc-800 p-11 rounded-2xl">
+              <div className="flex" key={item.id}>
+                {item.title}
+              </div>
+              <div className="flex" key={item.id}>
+                {item.body}
+              </div>
             </div>
-            <div className="flex" key={item.id}>
-              {item.body}
-            </div>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+      <div className="flex flex-[8] h-screen items-center align-middle justify-center">
+        <div className="flex flex-col flex-[8]  items-center align-middle justify-center bg-zinc-800 p-11 rounded-2xl space-y-8">
+          <h1 className="text-3xl font-extrabold text-white">Add a new Post</h1>
+          <div className="form flex flex-col space-y-3">
+            <input className="border border-white-950 bg-inherit rounded-xl p-2 text-white"></input>
+            <textarea className="border border-white-950 bg-inherit rounded-xl p-2 text-white"></textarea>
           </div>
-        ))
-      ) : (
-        <p>No data available</p>
-      )}
+        </div>
+      </div>
     </div>
   );
 }
